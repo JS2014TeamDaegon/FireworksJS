8c547f8280726c9721752dda8d938c69ba4a0b7f
ventsislav-georgiev <ventsislav.georgiev@live.com>
2014-06-15 14:34:01 +0300

    Project name changed from "JSApp" to "FireworksJS"

--- JSApp/js/fireworks-js/loader/loader.js
+++ JSApp/js/fireworks-js/loader/loader.js
@@ -1,6 +1,6 @@
 ﻿(function (fireworks, window) {
     var script = document.createElement('script');
-    script.src = "https://rawgithub.com/JS2014TeamDaegon/JSApp/master/JSApp/js/fireworks-js/release/fireworks-js.min.js";
+    script.src = "https://rawgithub.com/JS2014TeamDaegon/JSApp/master/FireworksJS/js/fireworks-js/release/fireworks-js.min.js";
     //script.src = "http://localhost:5039/js/fireworks-js/release/fireworks-js.min.js";
 
     script.onload = function () {
